9ab9c2f2364a7d67e7ff3cfaa41f7437ef2cbec0
Fray León Osorio Rivera <frayosorio@gmail.com>
2024-05-09 09:18:04 -0500

    Se agrega funcionalidad para agregar y modificar selecciones

--- src/app/features/componentes/seleccion/seleccion.component.ts
+++ src/app/features/componentes/seleccion/seleccion.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { ReferenciasMaterialModule } from '../../../shared/modulos/referencias-material.module';
 import { Seleccion } from '../../../core/entidades/Seleccion';
-import { NgxDatatableModule } from '@swimlane/ngx-datatable';
+import { ColumnMode, NgxDatatableModule, SelectionType } from '@swimlane/ngx-datatable';
 import { FormsModule } from '@angular/forms';
 import { SeleccionService } from '../../servicios/seleccion.service';
+import { MatDialog } from '@angular/material/dialog';
+import { SeleccionEditarComponent } from '../seleccion-editar/seleccion-editar.component';
 
 @Component({
   selector: 'app-seleccion',
@@ -23,11 +25,24 @@ export class SeleccionComponent {
     { name: "Nombre", prop: "nombre" },
     { name: "Entidad", prop: "entidad" },
   ];
+  public modoColumna = ColumnMode;
+  public tipoSeleccion = SelectionType;
 
-  constructor(private seleccionServicio: SeleccionService) {
+  public textoBusqueda: string = "";
+
+  public seleccionEscogida: Seleccion | undefined;
+
+  constructor(private seleccionServicio: SeleccionService,
+    private servicioDialog: MatDialog
+  ) {
     this.listar();
   }
 
+  public escoger(event: any) {
+    if (event.type == "click") {
+      this.seleccionEscogida = event.row;
+    }
+  }
 
   public listar() {
     this.seleccionServicio.listar().subscribe({
@@ -46,6 +61,95 @@ export class SeleccionComponent {
         window.alert(error.message);
       }
     });
+  }
+
+  buscar() {
+    if (this.textoBusqueda) {
+      this.ejecutarBusqueda(this.textoBusqueda);
+    }
+    else {
+      this.listar();
+    }
+  }
+
+  ejecutarBusqueda(texto: string) {
+    this.seleccionServicio.buscar(texto).subscribe({
+      next: response => {
+        this.selecciones = response;
+      },
+      error: error => {
+        window.alert(error.message);
+      }
+    });
+  }
+
+  agregar() {
+    const edicionSeleccion = this.servicioDialog.open(SeleccionEditarComponent, {
+      width: "500px",
+      height: "300px",
+      data: {
+        encabezado: "Agregando una Selección de Fútbol",
+        seleccion: {
+          id: 0,
+          nombre: "",
+          entidad: ""
+        }
+      },
+      disableClose: true,
+    });
+
+    edicionSeleccion.afterClosed().subscribe({
+      next: datos => {
+        if (datos) {
+          this.seleccionServicio.agregar(datos.seleccion).subscribe({
+            next: response => {
+              this.ejecutarBusqueda(datos.seleccion.nombre);
+            },
+            error: error => {
+              window.alert(error.message);
+            }
+          });
+        }
+      }
+    });
+  }
+
+  modificar() {
+    if (this.seleccionEscogida) {
+      const edicionSeleccion = this.servicioDialog.open(SeleccionEditarComponent, {
+        width: "500px",
+        height: "300px",
+        data: {
+          encabezado: `Editando Selección de Fútbol [${this.seleccionEscogida.nombre}]`,
+          seleccion: this.seleccionEscogida
+        },
+        disableClose: true,
+      });
+
+      edicionSeleccion.afterClosed().subscribe({
+        next: datos => {
+          if (datos) {
+            this.seleccionServicio.modificar(datos.seleccion).subscribe({
+              next: response => {
+                this.ejecutarBusqueda(datos.seleccion.nombre);
+              },
+              error: error => {
+                window.alert(error.message);
+              }
+            });
+          }
+        }
+      });
+    }
+    else {
+      window.alert("Debe escoger una Selección");
+    }
+  }
+  verificarEliminar() {
+
+  }
+  eliminar() {
+
   }
 
 }
--- src/app/features/servicios/seleccion.service.ts
+++ src/app/features/servicios/seleccion.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../../../environments/environment';
 import { Observable } from 'rxjs';
+import { Seleccion } from '../../core/entidades/Seleccion';
 
 @Injectable({
   providedIn: 'root'
@@ -16,6 +17,18 @@ export class SeleccionService {
 
   public listar(): Observable<any> {
     return this.http.get(`${this.url}listar`);
+  }
+
+  public agregar(seleccion: Seleccion): Observable<Seleccion> {
+    return this.http.post<Seleccion>(`${this.url}agregar`, seleccion);
+  }
+
+  public modificar(seleccion: Seleccion): Observable<Seleccion> {
+    return this.http.put<Seleccion>(`${this.url}modificar`, seleccion);
+  }
+
+  public buscar(textoBusqueda: string): Observable<Seleccion[]> {
+    return this.http.get<Seleccion[]>(`${this.url}buscar/${textoBusqueda}`);
   }
 
 }
